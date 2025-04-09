43482acac8584f1f19d2ed74dfa38771d890cdb8
PuhlMatias <matipuhl@gmail.com>
2025-04-09 20:09:36 -0300

    agregar

--- claseCliente.php
+++ claseCliente.php
@@ -1,4 +1,65 @@
 <?php
 class Cliente{
-    
+    //Atributos
+    private string $nombre;
+    private string $apellido;
+    private bool $estadoDeBaja;
+    private string $tipoDocumento;
+    private int $documento;
+
+    //Metodo constructor
+    public function __construct(string $nombre,string $apellido,bool $estadoDeBaja,string $tipoDocumento,int $documento )
+    {
+        $this->nombre=$nombre;
+        $this->apellido=$apellido;
+        $this->estadoDeBaja=$estadoDeBaja;
+        $this->tipoDocumento=$tipoDocumento;
+        $this->documento=$documento;
+    }
+
+    //METODOS DE ACCESO
+    //getters
+    public function getNombre(){
+        return $this->nombre;
+    }
+    public function getApellido(){
+        return $this->apellido;
+    }
+    public function getEstadoDeBaja(){
+        return $this->estadoDeBaja;
+    }
+    public function getTipoDocumento(){
+        return $this->tipoDocumento;
+    }
+    public function getDocumento(){
+        return $this->documento;
+    }
+
+    //setters
+    public function setNombre(string $nombre){
+        $this->nombre=$nombre;
+    }
+    public function setApellido(string $apellido){
+        $this->apellido=$apellido;
+    }
+    public function setEstadoDeBaja(bool $estadoDeBaja){
+        $this->estadoDeBaja=$estadoDeBaja;
+    }
+    public function setTipoDocumento(string $tipoDocumento){
+        $this->tipoDocumento=$tipoDocumento;
+    }
+    public function setDucumento(int $documento){
+        $this->documento=$documento;
+    }
+
+    //Metodo toString
+    public function __toString()
+    {
+        $estadoBaja = $this->getEstadoDeBaja() ? "Si esta dado de baja" : "No esta dado de baja";
+        return "Nombre: " . $this->getNombre().
+        "\nApellido: " . $this->getApellido().
+        "Estado de baja: " . $estadoBaja.
+        "Tipo de documento: " . $this->getTipoDocumento().
+        "Documento: " . $this->getDocumento();
+    }
 }
--- claseMoto.php
+++ claseMoto.php
@@ -1,4 +1,75 @@
 <?php
 class Moto{
-    
+    //Atributos
+    private int $codigo;
+    private float $costo;
+    private int $anioFabricacion;
+    private string $descripcion;
+    private float $incrementoAnual;
+    private bool $activa;
+
+    //Metodo constructor
+    public function __construct(int $codigo,float $costo,int $anioFabricacion,string $descripcion,float $incrementoAnual,bool $activa)
+    {
+        $this->codigo=$codigo;
+        $this->costo=$costo;
+        $this->anioFabricacion=$anioFabricacion;
+        $this->descripcion=$descripcion;
+        $this->incrementoAnual=$incrementoAnual;
+        $this->activa=$activa;
+    }
+
+    //METODOS DE ACCESO
+    //getters
+    public function getCodigo(){
+        return $this->codigo;
+    }
+    public function getCosto(){
+        return $this->costo;
+    }
+    public function getAnioFabricacion(){
+        return $this->anioFabricacion;
+    }
+    public function getDescripcion(){
+        return $this->descripcion;
+    }
+    public function getIncrementoAnual(){
+        return $this->incrementoAnual;
+    }
+    public function getActiva(){
+        return $this->activa;
+    }
+
+    //setters
+    public function setCodigo(int $codigo){
+        $this->codigo=$codigo;
+    }
+    public function setCosto(float $costo){
+        $this->costo=$costo;
+    }
+    public function setAnioFabricacion(int $anioFabricacion){
+        $this->anioFabricacion=$anioFabricacion;
+    }
+    public function setDescripcion(string $descripcion){
+        $this->descripcion=$descripcion;
+    }
+    public function setIncrementoAnual(float $incrementoAnual){
+        $this->incrementoAnual=$incrementoAnual;
+    }
+    public function setActiva(bool $activa){
+        $this->activa=$activa;
+    }
+
+    //Metodo toString
+    public function __toString()
+    {
+        $disponibilidad = $this->getActiva() ? "Esta disponible" : "NO esta disponible";
+        return "INFORMACION DE LA MOTO\n".
+        "Codigo: " . $this->getCodigo().
+        "Costo: ". $this->getCosto().
+        "Año de fabricacion: ". $this->getAnioFabricacion().
+        "Descripción: ". $this->getDescripcion().
+        "Incremenro anual: ". $this->getIncrementoAnual().
+        "Activa: ". $disponibilidad;
+    }
 }
